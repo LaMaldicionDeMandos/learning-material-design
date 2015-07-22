e5bee208ac269455d0df5753ca92fd66b6176d14
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-07-21 23:04:08 -0300

    setteo materials theme

--- learning-material-design/app/src/main/java/learning_material_design/android/pasut/org/learning_material_design/MainActivity.java
+++ learning-material-design/app/src/main/java/learning_material_design/android/pasut/org/learning_material_design/MainActivity.java
@@ -1,12 +1,13 @@
 package learning_material_design.android.pasut.org.learning_material_design;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
